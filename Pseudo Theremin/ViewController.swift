34cb3c38a465d4b5483bcd40418a991a76876e23
tkumata <tkumata@gmail.com>
2016-11-19 10:25:15 +0900

    Initial Commit

---
+++ Pseudo Theremin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pseudo Theremin
+//
+//  Created by KUMATA Tomokatsu on 2016/11/19.
+//  Copyright © 2016 KUMATA Tomokatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
